da2a1f9ab6c276cb6d74558657a4043f0c9df05e
住在冬天 <andygulin@hotmail.com>
2023-11-15 18:28:27 +0800

    Add CMD

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"DouBanMovieSpider/spider"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
@@ -8,16 +9,36 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "DouBanMovieSpider",
-	Short: "",
-	Long:  "",
-	Run: func(cmd *cobra.Command, args []string) {
-
-	},
+	Short: "Spider Douban movie/TV show information\n",
+	Long:  "Spider Douban movie/TV show information\n",
 }
 
 func Execute() {
+	rootCmd.AddCommand(infoCmd, storeCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Print spider information.",
+	Long:  "Print spider information.",
+	Run: func(cmd *cobra.Command, args []string) {
+		subjectId := args[0]
+		request := spider.Request{SubjectId: subjectId}
+		fmt.Println(request)
+	},
+}
+
+var storeCmd = &cobra.Command{
+	Use:   "store",
+	Short: "Store spider information to MongoDB.",
+	Long:  "Store spider information to MongoDB.",
+	Run: func(cmd *cobra.Command, args []string) {
+		subjectId := args[0]
+		request := spider.Request{SubjectId: subjectId}
+		fmt.Println(request)
+	},
 }
