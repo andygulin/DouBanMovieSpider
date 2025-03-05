335628af496f173eaa21775ebf110409de314a85
gulin <andygulin@hotmail.com>
2025-03-05 17:15:02 +0800

    upgrade

--- service/consts.go
+++ service/consts.go
@@ -2,4 +2,3 @@ package service
 
 const UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
 const Domain = "https://movie.douban.com"
-const DB = "douban"
--- service/mongo.go
+++ service/mongo.go
@@ -2,8 +2,14 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 const (
@@ -14,11 +20,26 @@ const (
 )
 
 var mongoDb *mongo.Client
+var DB string
 
 func NewMongoDb() (*mongo.Client, error) {
-	var err error
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	path = path[:index]
+	viper.SetConfigFile(path + "/conf/conf.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	host := viper.GetString("mongo.host")
+	port := viper.GetInt("mongo.port")
+	DB = viper.GetString("mongo.db")
+	uri := fmt.Sprintf("mongodb://%s:%d", host, port)
+
 	if mongoDb == nil {
-		clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+		clientOptions := options.Client().ApplyURI(uri)
 		mongoDb, err = mongo.Connect(context.TODO(), clientOptions)
 		if err != nil {
 			return nil, err
